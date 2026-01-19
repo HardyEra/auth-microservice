420afb4e6040970605d1c2f690aab689f39e2143
HardyEra <kashyap.hardik789@gmail.com>
2026-01-19 23:39:05 +0530

    mongodb & swagger add

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 import express from "express";
 import cors from "cors";
 import clientRoutes from "./routes/client.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import protectedRoutes from "./routes/protected.routes.js";
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get("/health", (req, res) => {
   res.status(200).json({
     status: "UP",
